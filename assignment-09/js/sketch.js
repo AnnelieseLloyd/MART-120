7be7246d1e65b7663d482070bfef14b51361ae17
AnnelieseLloyd <89812541+AnnelieseLloyd@users.noreply.github.com>
2022-03-20 16:54:17 -0600

    assignment-09

---
+++ assignment-09/js/sketch.js
@@ -0,0 +1,40 @@
+function setup()
+{
+  createCanvas(500,600);
+}
+function draw()
+{
+  background(120,45,78);
+  text('Anneliese in 2D', 100, 410);
+  fill(255,255,255);
+  textSize(10);
+  fill(210, 180, 140);
+  line(320, 380, 255, 295)
+  line(140, 360, 255, 295)
+  line(140, 160, 205, 195)
+  line(240, 260, 305, 135)
+  rect(215,100,50,70);
+  circle(240,70,125);
+  line(240, 100, 205, 95)
+  strokeWeight(10);
+  stroke('blue');
+  point(210, 55);
+  point(260, 55);
+  stroke('brown');
+  line(210, 20, 150, 105)
+  line(200, 30, 140, 105)
+  line(260, 10, 380, 105)
+  line(260, 20, 370, 105)
+  line(200, 20, 270, 5)
+  strokeWeight(1);
+  stroke('black');
+  fill(0,255,255);
+  triangle(80, 325, 240, 170, 375, 325);
+  square(210,150,60);
+  text('By: Anneliese Lloyd', 300, 510);
+  textSize(40);
+  fill(0,255,255);
+
+
+
+}
